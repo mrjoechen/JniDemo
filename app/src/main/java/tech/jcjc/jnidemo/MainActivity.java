abb3a07f0a9f9767ac3506b8225bb617fef6d484
chenqiao <chenqiao@thunder.com.cn>
2018-01-02 09:39:57 +0800

    .

--- app/src/main/java/tech/jcjc/jnidemo/MainActivity.java
+++ app/src/main/java/tech/jcjc/jnidemo/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // Example of a call to a native method
-        TextView tv = (TextView) findViewById(R.id.sample_text);
+        TextView tv =  findViewById(R.id.sample_text);
 
         NdkJniUtils ndkJniUtils = new NdkJniUtils();
         tv.setText(ndkJniUtils.stringFromJNI());
